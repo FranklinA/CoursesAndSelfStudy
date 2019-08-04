e67219084d01bd09054487b95bedbd190c608e51
Franklin A <angulofranklin@hotmail.com>
2019-08-04 12:12:24 -0300

    Change the path with file ~/.bashrc and file1err print the error for 'cat foo'

--- LinuxSystemsProgramming/01/1err.c
+++ LinuxSystemsProgramming/01/1err.c
@@ -1,11 +1,17 @@
-#include <fcnt1.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-...
+
+void main()
+{
+	int fd;
 	fd = open("foo",O_WRONLY | O_CREAT, 0644);
 	if (fd<0){/*Typically, system calls return -1 on failure*/
 		printf("error number %d\n",errno);/* They set the global variable errno*/
 		perror("foo");
 		exit(1);
 	}
+	write(fd,"hello world",11);
+	close(fd);
+}
