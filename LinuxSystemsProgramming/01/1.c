3277195894951dfb0da7cc60249323e29cb77c00
Franklin A <angulofranklin@hotmail.com>
2019-08-04 10:49:40 -0300

    Basic example 1.c

---
+++ LinuxSystemsProgramming/01/1.c
@@ -0,0 +1,11 @@
+#include<fcnt1.h> /*Symbolic constants defined here*/
+
+void main()/*The entry point for a C program*/
+{
+	int fd;
+	/*open it is our first system call,O_WRONLY is Write only,O_CREAT Create file if it doesn't exit ,0644 Access permissions of new file (rw-r--r--)*/
+	fd = open("foo", O_WRONLY | O_CREAT, 0644);
+	/**/
+	write(fd, "hello word",11);
+	close(fd);
+}
