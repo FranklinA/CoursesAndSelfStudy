bda13f718c05f88379010e5bc6902cb11842ce58
Franklin A <angulofranklin@hotmail.com>
2019-08-20 10:47:46 -0300

    Escribir en  archivo de texto llamado datos.txt

--- PythonScript/manejoArchivo.py
+++ PythonScript/manejoArchivo.py
@@ -2,6 +2,12 @@ def crearArchivo():
 	archivo=open('datos.txt','w')
 	archivo.close()
 
-crearArchivo()
+def escribirArchivo():
+	archivo=open('datos.txt','a')#Modo de appned para NO sobreesribir el archivo sinorespetar su contenido previo.
+	archivo.write('Franklin Angulo\n')
+	archivo.write('95959559')
+	archivo.close
 
+
+escribirArchivo()
 
